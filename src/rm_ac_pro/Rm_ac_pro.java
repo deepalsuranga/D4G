6936a8e36d69b3be0d86dbd804e95eaba31feaba
Deepal Suranga Weerasuriya <deepal.suranga29@gmail.com>
2015-12-23 00:12:42 +0530

    project start. create None helo world ->Hello! run it. no error. output:Rename Project (rm_ac_pro) 100%

---
+++ src/rm_ac_pro/Rm_ac_pro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rm_ac_pro;
+
+/**
+ *
+ * @author deepal_suranga
+ */
+public class Rm_ac_pro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
