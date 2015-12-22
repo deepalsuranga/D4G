0b562a32a27370374f682ae5ac794aabd9c8859c
Deepal Suranga Weerasuriya <deepal.suranga29@gmail.com>
2015-12-23 00:17:50 +0530

    project start. create None //Comment Only run it. no error. output:No Comment 100%

--- src/rm_ac_pro/Rm_ac_pro.java
+++ src/rm_ac_pro/Rm_ac_pro.java
@@ -15,7 +15,7 @@ public class Rm_ac_pro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Deepal
     }
     
 }
